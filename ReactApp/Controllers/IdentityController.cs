8e2c2bd765ce66784f9b23eecf18ea18a1871c50
Pollyus <92532493+Pollyus@users.noreply.github.com>
2023-04-25 12:44:32 +0300

    .

--- ReactApp/Controllers/IdentityController.cs
+++ ReactApp/Controllers/IdentityController.cs
@@ -1,51 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ReactApp.ViewModels;
-using ReactApp.Services.Interfaces;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using ReactApp.ViewModels;
+//using ReactApp.Services.Interfaces;
 
-namespace ReactApp.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class IdentityController : ControllerBase
-    {
-        private readonly IIdentityService _userService;
+//namespace ReactApp.Controllers
+//{
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class IdentityController : ControllerBase
+//    {
+//        private readonly IIdentityService _userService;
 
-        public IdentityController(IIdentityService userService)
-        {
-            _userService = userService;
-        }
+//        public IdentityController(IIdentityService userService)
+//        {
+//            _userService = userService;
+//        }
 
-        [HttpPost("authenticate")]
-        public IActionResult Authenticate(IdentityViewModel model)
-        {
-            var response = _userService.Authenticate(model);
+//        [HttpPost("authenticate")]
+//        public IActionResult Authenticate(IdentityViewModel model)
+//        {
+//            var response = _userService.Authenticate(model);
 
-            if (response == null)
-                return BadRequest(new { message = "Username or password is incorrect" });
+//            if (response == null)
+//                return BadRequest(new { message = "Username or password is incorrect" });
 
-            return Ok(response);
-        }
+//            return Ok(response);
+//        }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(UserViewModel userModel)
-        {
-            var response = await _userService.Register(userModel);
+//        [HttpPost("register")]
+//        public async Task<IActionResult> Register(UserViewModel userModel)
+//        {
+//            var response = await _userService.Register(userModel);
 
-            if (response == null)
-            {
-                return BadRequest(new { message = "Didn't register!" });
-            }
+//            if (response == null)
+//            {
+//                return BadRequest(new { message = "Didn't register!" });
+//            }
 
-            return Ok(response);
-        }
+//            return Ok(response);
+//        }
 
-        [Helpers.Authorize]
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var users = _userService.GetAll();
-            return Ok(users);
-        }
+//        [Helpers.Authorize]
+//        [HttpGet]
+//        public IActionResult GetAll()
+//        {
+//            var users = _userService.GetAll();
+//            return Ok(users);
+//        }
 
-    }
-}
+//    }
+//}
