40fbe6c816e628eaa65916be70416dbab9ecac36
Pollyus <92532493+Pollyus@users.noreply.github.com>
2023-04-25 12:38:19 +0300

    .

--- DBRepository/Repositories/IdentityRepository.cs
+++ DBRepository/Repositories/IdentityRepository.cs
@@ -11,36 +11,45 @@ namespace DBRepository.Repositories
 {
     public class IdentityRepository<T> : IIdentityRepository<T> where T: BaseEntity 
     {
-        private readonly RepositoryDbContext _context;
+        public IdentityRepository(string connectionString, IRepositoryContextFactory contextFactory) : base(connectionString, contextFactory) { }
 
-        public IdentityRepository(RepositoryDbContext context)
+        public async Task<User> GetUser(string userName)
         {
-            _context = context;
-        }
-
-        public List<T> GetAll()
-        {
-            return _context.Set<T>().ToList();
-        }
-
-        public T GetById(long id)
-        {
-            var result = _context.Set<T>().FirstOrDefault(x => x.Id == id);
-
-            if (result == null)
+            using (var context = ContextFactory.CreateDbContext(ConnectionString))
             {
-                //todo: need to add logger
-                return null;
+                return await context.Users.FirstOrDefaultAsync(u => u.Login == userName);
             }
-
-            return result;
         }
+        //private readonly RepositoryDbContext _context;
 
-        public async Task<long> Add(T entity)
-        {
-            var result = await _context.Set<T>().AddAsync(entity);
-            await _context.SaveChangesAsync();
-            return result.Entity.Id;
-        }
+        //public IdentityRepository(RepositoryDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //public List<T> GetAll()
+        //{
+        //    return _context.Set<T>().ToList();
+        //}
+
+        //public T GetById(long id)
+        //{
+        //    var result = _context.Set<T>().FirstOrDefault(x => x.Id == id);
+
+        //    if (result == null)
+        //    {
+        //        //todo: need to add logger
+        //        return null;
+        //    }
+
+        //    return result;
+        //}
+
+        //public async Task<long> Add(T entity)
+        //{
+        //    var result = await _context.Set<T>().AddAsync(entity);
+        //    await _context.SaveChangesAsync();
+        //    return result.Entity.Id;
+        //}
     }
 }
