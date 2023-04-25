33cb781ede62ab6a375f1f23fe5e7474d10e231a
Pollyus <92532493+Pollyus@users.noreply.github.com>
2023-04-25 12:44:26 +0300

    .

--- ReactApp/Services/Implementation/IdentityService.cs
+++ ReactApp/Services/Implementation/IdentityService.cs
@@ -1,64 +1,85 @@
 ﻿using AutoMapper;
+//using DBRepository.Interfaces;
+//using Microsoft.Extensions.Configuration;
+//using Models;
+//using ReactApp.Helpers;
+//using ReactApp.Services.Interfaces;
+//using ReactApp.ViewModels;
+
+//namespace ReactApp.Services.Implementation
+//{
+//    public class IdentityService : IIdentityService
+//    {
+//        private readonly IIdentityRepository<User> _userRepository;
+//        private readonly IConfiguration _configuration;
+//        private readonly IMapper _mapper;
+//        //private readonly IJwtManager _jwtManager;
+
+//        public IdentityService(IIdentityRepository<User> repository, IConfiguration configuration, IMapper mapper)
+//        {
+//            _userRepository = repository;
+//            _configuration = configuration;
+//            _mapper = mapper;
+
+//        }
+//        public IEnumerable<User> GetAll()
+//        {
+//            return _userRepository.GetAll();
+//        }
+//        public User GetById(int id)
+//        {
+//            return _userRepository.GetById(id);
+//        }
+//        public AuthenticateResponse Authenticate(IdentityViewModel model)
+//        {
+//            var user = _userRepository
+//                .GetAll()
+//                .FirstOrDefault(x => x.UserName == model.Username && x.Password == model.Password);
+
+//            if (user == null)
+//            {
+//                // todo: need to add logger
+//                return null;
+//            }
+
+//            var token = _configuration.GenerateJwtToken(user);
+
+//            return new AuthenticateResponse(user, token);
+//        }
+//        public async Task<AuthenticateResponse> Register(UserViewModel userModel)
+//        {
+//            var user = _mapper.Map<User>(userModel);
+
+//            var addedUser = await _userRepository.Add(user);
+
+//            var response = Authenticate(new IdentityViewModel
+//            {
+//                Username = user.UserName,
+//                Password = user.Password
+//            });
+
+//            return response;
+//        }
+//    }
+//}
 using DBRepository.Interfaces;
-using Microsoft.Extensions.Configuration;
 using Models;
-using ReactApp.Helpers;
 using ReactApp.Services.Interfaces;
-using ReactApp.ViewModels;
 
 namespace ReactApp.Services.Implementation
 {
     public class IdentityService : IIdentityService
     {
-        private readonly IIdentityRepository<User> _userRepository;
-        private readonly IConfiguration _configuration;
-        private readonly IMapper _mapper;
-        //private readonly IJwtManager _jwtManager;
+        IIdentityRepository _repository;
 
-        public IdentityService(IIdentityRepository<User> repository, IConfiguration configuration, IMapper mapper)
+        public IdentityService(IIdentityRepository repository)
         {
-            _userRepository = repository;
-            _configuration = configuration;
-            _mapper = mapper;
-
+            _repository = repository;
         }
-        public IEnumerable<User> GetAll()
+
+        public async Task<User> GetUser(string userName)
         {
-            return _userRepository.GetAll();
-        }
-        public User GetById(int id)
-        {
-            return _userRepository.GetById(id);
-        }
-        public AuthenticateResponse Authenticate(IdentityViewModel model)
-        {
-            var user = _userRepository
-                .GetAll()
-                .FirstOrDefault(x => x.UserName == model.Username && x.Password == model.Password);
-
-            if (user == null)
-            {
-                // todo: need to add logger
-                return null;
-            }
-
-            var token = _configuration.GenerateJwtToken(user);
-
-            return new AuthenticateResponse(user, token);
-        }
-        public async Task<AuthenticateResponse> Register(UserViewModel userModel)
-        {
-            var user = _mapper.Map<User>(userModel);
-
-            var addedUser = await _userRepository.Add(user);
-
-            var response = Authenticate(new IdentityViewModel
-            {
-                Username = user.UserName,
-                Password = user.Password
-            });
-
-            return response;
+            return await _repository.GetUser(userName);
         }
     }
 }
--- ReactApp/Services/Interfaces/IIdentityService.cs
+++ ReactApp/Services/Interfaces/IIdentityService.cs
@@ -5,9 +5,10 @@ namespace ReactApp.Services.Interfaces
 {
     public interface IIdentityService
     {
-        AuthenticateResponse Authenticate(IdentityViewModel model);
-        Task<AuthenticateResponse> Register(UserViewModel userModel);
-        IEnumerable<User> GetAll();
-        User GetById(int id);
+        //AuthenticateResponse Authenticate(IdentityViewModel model);
+        //Task<AuthenticateResponse> Register(UserViewModel userModel);
+        //IEnumerable<User> GetAll();
+        //User GetById(int id);
+        Task<User> GetUser(string name);
     }
 }
